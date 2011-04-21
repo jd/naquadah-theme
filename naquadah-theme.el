a047fdc01140365f24fd7ebcd83e068b0f9c972a
Julien Danjou <julien@danjou.info>
2011-04-21 14:02:49 +0200

    Fix colors

--- naquadah-theme.el
+++ naquadah-theme.el
@@ -264,12 +264,12 @@
    `(magit-log-graph ((t (:foreground ,aluminium-2))))
    `(magit-item-highlight ((t (:background ,aluminium-6))))
    `(magit-item-mark ((t (:foreground ,orange-1))))
-   `(magit-log-tag-label ((t (:background ,chameleon-3 :box t))))
+   `(magit-log-tag-label ((t (:background ,chameleon-3 :box t :foreground ,black))))
    `(magit-log-head-label-bisect-good ((t (:background ,chameleon-2 :box t))))
    `(magit-log-head-label-bisect-bad ((t (:background ,scarlet-red-3 :box t))))
-   `(magit-log-head-label-remote ((t (:background ,butter-2 :box t))))
+   `(magit-log-head-label-remote ((t (:background ,butter-2 :box t :foreground ,black))))
    '(magit-log-head-label-tags ((t (:inherit (magit-log-tag-label)))))
-   `(magit-log-head-label-local ((t (:foreground ,sky-blue-1 :background ,aluminium-5
+   `(magit-log-head-label-local ((t (:foreground ,black :background ,sky-blue-1
                                                  :box t))))
 
 
