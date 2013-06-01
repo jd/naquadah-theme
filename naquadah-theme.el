aa63f0a09dbc63ede5ea909eea590acd1fe86fa8
Stephan Creutz <stephan.cr@gmx.de>
2013-06-01 15:27:18 +0200

    check if custom-theme-load-path is void
    custom-theme-load-path is introduced in emacs 24 and won't work in earlier
    versions
    
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- naquadah-theme.el
+++ naquadah-theme.el
@@ -615,7 +615,7 @@
  '(jabber-title-small (:weight bold :height 1.0)))
 
 ;;;###autoload
-(when load-file-name
+(when (and (boundp 'custom-theme-load-path) load-file-name)
   (add-to-list 'custom-theme-load-path
                (file-name-as-directory (file-name-directory load-file-name))))
 
